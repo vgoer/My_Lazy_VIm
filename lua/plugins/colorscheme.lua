e02650133fda2b53d4a0acc02d4b09fb4682fc49
vgoer <goerblue@gmail.com>
2024-09-22 16:20:41 +0800

    add: solorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "LazyVim/Lazyvim",
+    opts = {
+      colorscheme = "tokyonight-moon",
+    },
+  },
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "olimorris/onedarkpro.nvim",
+  },
+  {
+    "savq/melange-nvim",
+  },
+}
