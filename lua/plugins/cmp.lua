26d6af9fc902268aec4c9703084af92a2bb8f105
vgoer <goerblue@gmail.com>
2024-09-22 16:20:22 +0800

    add: cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,19 @@
+return {
+
+  {
+    "hrsh7th/cmp-cmdline",
+    config = function()
+      local cmp = require("cmp")
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+        matching = { disallow_symbol_nonprefix_matching = false },
+      })
+    end,
+  },
+}
