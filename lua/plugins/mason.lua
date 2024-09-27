669a3c6e57df024836566257b653aae1c84fc5f4
vgoer <goerblue@gmail.com>
2024-09-27 17:45:09 +0800

    change: lazy mason 提示插件

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -3,7 +3,12 @@ return {
     "williamboman/mason-lspconfig.nvim",
     opts = {
       ensure_installed = {
+        -- rust 提示插件
         "rust_analyzer",
+
+        -- html 提示插件
+        -- "html-lsp",
+        -- css 提示插件
       },
     },
     config = function() end,
