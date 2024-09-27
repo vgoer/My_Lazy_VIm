17a204898373804e0716c5f2c41162d0f7ff97cb
vgoer <goerblue@gmail.com>
2024-09-27 17:44:04 +0800

    change: lazy 前端插件

--- lua/plugins/frontend.lua
+++ lua/plugins/frontend.lua
@@ -14,13 +14,29 @@ return {
   {
     "mattn/emmet-vim",
     ft = { "html", "css", "javascript", "typescript", "jsx", "tsx" },
-    -- keys = {
-    --   {
-    --     "<C-k", -- 快捷键
-    --     mode = { "i", "n" }, -- 模式
-    --     "<plug>(emmet-expand-abbr)", -- Emmet 的扩展缩写命令
-    --     desc = "Emmet 提示", -- 描述信息
-    --   },
-    -- },
+  },
+  -- 彩虹缩进
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = function(_, opts)
+      -- Other blankline configuration here
+      return require("indent-rainbowline").make_opts(opts)
+    end,
+    dependencies = {
+      "TheGLander/indent-rainbowline.nvim",
+    },
+  },
+  -- 颜色高亮
+  {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      require("nvim-highlight-colors").setup({
+        -- 自动显示颜色代码
+        enable_named_colors = true,
+        -- 启用 tailwindcss
+        enable_tailwind = true,
+        -- 可以根据需要添加更多自定义设置
+      })
+    end,
   },
 }
