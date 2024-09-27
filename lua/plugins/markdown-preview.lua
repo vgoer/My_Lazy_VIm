041e76c95344da101a2e932972041d76c37fc2e1
vgoer <goerblue@gmail.com>
2024-09-27 17:44:37 +0800

    change: lazy markdown预览

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,19 +1,9 @@
+-- markdown 预览插件
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
   build = function()
-    require("lazy").load({ plugins = { "markdown-preview.nvim" } })
     vim.fn["mkdp#util#install"]()
-  end,
-  keys = {
-    {
-      "<leader>cp",
-      ft = "markdown",
-      "<cmd>MarkdownPreviewToggle<cr>",
-      desc = "文档查看",
-    },
-  },
-  config = function()
-    vim.cmd([[do FileType]])
   end,
 }
