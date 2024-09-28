73323c8a9abc84969c2210ecd665d2f4d75c9b68
vgoer <goerblue@gmail.com>
2024-09-28 08:19:48 +0800

    add: nvim 彩虹缩进

---
+++ lua/plugins/rainbow.lua
@@ -0,0 +1,12 @@
+-- 彩虹括号
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = { "HiPhish/nvim-ts-rainbow2" },
+  opts = function(_, opts)
+    opts.rainbow = {
+      enable = true,
+      query = "rainbow-parens",
+      strategy = require("ts-rainbow").strategy.global,
+    }
+  end,
+}
