e5ade3c73734be2cd7971483b77b1e5ff8e17f24
vgoer <goerblue@gmail.com>
2024-09-28 08:19:22 +0800

    add: nvim 代码片段

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,38 @@
+-- 代码片段插件
+return {
+  "L3MON4D3/LuaSnip",
+  lazy = true,
+  build = (not LazyVim.is_win())
+      and "echo 'NOTE: jsregexp is optional, so not a big deal if it fails to build'; make install_jsregexp"
+    or nil,
+  dependencies = {
+    {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        -- 插入的代码片段  位置。
+        require("luasnip.loaders.from_lua").load({
+          paths = { "~/.config/nvim/lua/plugins/snips" },
+        })
+      end,
+    },
+    {
+      "nvim-cmp",
+      dependencies = {
+        "saadparwaiz1/cmp_luasnip",
+      },
+      opts = function(_, opts)
+        opts.snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        }
+        table.insert(opts.sources, { name = "luasnip" })
+      end,
+    },
+  },
+  opts = {
+    history = true,
+    delete_check_events = "TextChanged",
+  },
+}
