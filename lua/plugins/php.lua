ba37e29a11ae8fe1abab497fa63a204e054d1ea4
vgoer <goerblue@gmail.com>
2024-09-28 08:20:19 +0800

    add: nvim php环境

---
+++ lua/plugins/php.lua
@@ -0,0 +1,17 @@
+-- php 开发插件
+return {
+  -- -- PHP Syntax Highlighting and PHP Support
+  -- {
+  --   "stephpy/vim-php-cs-fixer",
+  --   event = "BufWritePre",
+  --   ft = { "php" },
+  --   config = function()
+  --     vim.cmd([[autocmd BufWritePre *.php execute ':call PhpCsFixerFixFile()']])
+  --   end,
+  -- },
+  -- -- PHP Syntax
+  -- {
+  --   "StanAngeloff/php.vim",
+  --   ft = { "php" },
+  -- },
+}
