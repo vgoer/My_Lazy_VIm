ba94f5e63441004208d41fd4d3730845b3d0c6d6
vgoer <goerblue@gmail.com>
2024-09-29 10:46:40 +0800

    add: 添加java的snips

---
+++ lua/plugins/snips/java.lua
@@ -0,0 +1,8 @@
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local c = ls.choice_node
+local d = ls.dynamic_node
